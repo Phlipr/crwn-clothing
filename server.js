0d008ba2edaed1739dd559ca38ba84123ea94ab2
Phillip Reynolds <preynolds@atp.com>
2021-10-15 17:36:32 -0500

    Fixed typo in server.js file.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__direname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
